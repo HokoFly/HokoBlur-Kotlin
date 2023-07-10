df2ff33a12ff17f8128a549b6f954a0945264651
HokoFly <yuxfzju@gmail.com>
2023-07-10 17:26:44 +0800

    Fix the boundary distortion when use a bigger sample factor

--- library/src/main/java/com/hoko/ktblur/ext/BitmapExtension.kt
+++ library/src/main/java/com/hoko/ktblur/ext/BitmapExtension.kt
@@ -1,17 +1,23 @@
 package com.hoko.ktblur.ext
 
 import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Matrix
+import android.graphics.Paint
 
 fun Bitmap.scale(factor: Float): Bitmap {
     if (factor == 1.0f) {
         return this
     }
-    val ratio = 1f / factor
-    val matrix = Matrix().apply {
-        postScale(ratio, ratio)
-    }
-    return Bitmap.createBitmap(this, 0, 0, this.width, this.height, matrix, true)
+    val scale = 1.0f / factor
+    val newWidth: Int = (width * scale).toInt()
+    val newHeight: Int = (height * scale).toInt()
+    val scaledBitmap = Bitmap.createBitmap(newWidth, newHeight, getConfig())
+    val canvas = Canvas(scaledBitmap)
+    val scaleMatrix = Matrix()
+    scaleMatrix.setScale(scale, scale)
+    canvas.drawBitmap(this, scaleMatrix, Paint())
+    return scaledBitmap
 }
 
 fun Bitmap.translate(translateX: Int, translateY: Int): Bitmap {
