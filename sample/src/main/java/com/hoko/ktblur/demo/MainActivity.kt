bee6aa09887959f8cd99d5facafd2a5e9196fb40
HokoFly <yuxfzju@gmail.com>
2023-09-04 20:37:29 +0800

    Remove main page background

--- sample/src/main/java/com/hoko/ktblur/demo/MainActivity.kt
+++ sample/src/main/java/com/hoko/ktblur/demo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 
